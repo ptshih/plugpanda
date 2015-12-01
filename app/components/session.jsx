e91ab8aafe4ae8ff6a6f4d0043aefcb19d13980f
Peter Shih <ptshih@gmail.com>
2015-12-01 08:45:03 -0800

    housekeeping

--- app/components/session.jsx
+++ app/components/session.jsx
@@ -15,7 +15,6 @@ import Nav from './nav';
 import Table from './table';
 import GoogleMap from './google-map';
 import Highcharts from 'react-highcharts/bundle/highcharts';
-import HighchartsMore from 'react-highcharts/dist/modules/more';
 
 // Mixins
 import Fetch from '../mixins/fetch';
