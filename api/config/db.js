830d8a4304ad0bb7c83d8a49f3deade969d4b372
Peter Shih <ptshih@gmail.com>
2016-02-07 02:36:53 -0800

    Fix mongodb connection string to Compose.io mongos

--- api/config/db.js
+++ api/config/db.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = new Muni.Mongo(
-  `mongodb://${nconf.get('PANDA_MONGODB_USER')}:${nconf.get('PANDA_MONGODB_PASSWORD')}@aws-us-east-1-portal.4.dblayer.com:11115,aws-us-east-1-portal.9.dblayer.com:10549/production`,
+  `mongodb://${nconf.get('PANDA_MONGODB_USER')}:${nconf.get('PANDA_MONGODB_PASSWORD')}@aws-us-east-1-portal.4.dblayer.com:11115/production`,
   {
     mongos: {
       ssl: true,
