6d837a6a09042bb66978e28b18ac6519a1941d24
Peter Shih <ptshih@gmail.com>
2016-02-10 17:55:27 -0800

    copy

--- app/components/faq.jsx
+++ app/components/faq.jsx
@@ -9,11 +9,23 @@ export default React.createClass({
 
   render() {
     return (
-      <div>
-        <section>
-          Magic!
-        </section>
-      </div>
+      <section>
+        <h4>How Plug Panda Works</h4>
+
+        <p className="lead">PlugPanda monitors your ChargePoint charging sessions and automatically stops them for you.</p>
+        <div>[IMAGE]</div>
+
+        <br/>
+
+        <p className="lead">Set a configurable threshold for when to stop trickle charging.</p>
+        <p>NOTE: If a charging session is free, Plug Panda does not attempt to stop the session prematurely.</p>
+        <div>[IMAGE]</div>
+
+        <br/>
+
+        <p className="lead">Keep track of how much money you're saving.</p>
+        <div>[IMAGE]</div>
+      </section>
     );
   },
 
--- app/components/root.jsx
+++ app/components/root.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 // Components
 
@@ -10,7 +11,34 @@ export default React.createClass({
   render() {
     return (
       <section>
-        Welcome to Plug Panda! More news to come soon!
+        <h4>What’s wrong with public EV charging today?</h4>
+
+        <p>
+          <a href="http://www.chargepoint.com/">ChargePoint</a> is the largest provider of public (paid and free) EV charging stations.
+          <br/>
+          It's also the most commonly installed EV charging station in apartment complexes.
+        </p>
+
+        <p className="lead">Many ChargePoint stations charge by the hour:</p>
+
+        <ul>
+          <li>The rates (often $1-2/hr) are billed as long as the car is connected to the charger.</li>
+          <li>Even if the car is fully charged, billing will continue until the session is ended.</li>
+          <li>Sessions can currently be ended by physically unplugging the car or by using their mobile app.</li>
+        </ul>
+
+        <p className="lead">There are 2 problems with this:</p>
+        <div><strong>1. If you forget to unplug your car when it is fully charged, you will continue to be billed.</strong></div>
+        <div>This is particularly bad for overnight charging in apartment complexes.</div>
+
+        <br/>
+
+        <div><strong>2. Like smartphones, the charge rate of EVs trickles down as it approaches 100%.</strong></div>
+        <div>The reduced rate of charging could be as much as 20% of regular rate towards the last 5-10% of the battery.</div>
+
+        <br/>
+
+        <p>If this sounds like your situation today, <Link to="/faq">read more about how Plug Panda can help!</Link></p>
       </section>
     );
   },
